f854edcccd45fac04e7ce9969043cb116908fc63
jvance <jvance@redhat.com>
2014-03-19 07:50:15 -0700

    bump version

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@
 
 # set global variables
 SCRIPT=$(basename $0)
-INSTALL_VER='0.77'   # self version
+INSTALL_VER='0.78'   # self version
 INSTALL_DIR=$PWD     # name of deployment (install-from) dir
 INSTALL_FROM_IP=($(hostname -I))
 INSTALL_FROM_IP=${INSTALL_FROM_IP[$(( ${#INSTALL_FROM_IP[@]}-1 ))]} # last ntry
